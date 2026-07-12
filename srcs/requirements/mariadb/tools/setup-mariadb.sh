cd963dd65a3627a9e6b2d064bc16c16b822df872
Nour Fakih <fakihnourzahraa@gmail.com>
2026-07-12 18:24:32 +0300

    new

--- srcs/requirements/mariadb/tools/setup-mariadb.sh
+++ srcs/requirements/mariadb/tools/setup-mariadb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -e
 
-#tells mariadb to listen on all networks
+#tells mariadb to listen on all networks, not just inside the container
 echo "[mysqld]
 bind-address = 0.0.0.0" > /etc/mysql/mariadb.conf.d/99-custom.cnf
 
@@ -9,17 +8,19 @@ if [ ! -d /var/lib/mysql/mysql ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-mysqld_safe --skip-syslog &
-MYSQL_PID=$!
+mysqld_safe & MYSQL_PID=$!
+# my sql is a server, meaning it runs forever, so the & is to intialize the rest of the script
 
 echo "Waiting for MariaDB to start..."
-until mysqladmin ping -u root --silent 2>/dev/null || mysqladmin ping -u root -p"$DB_ROOT_PASSWORD" --silent 2>/dev/null; do
+until mysqladmin ping -u root --silent 2>/dev/null; do
     sleep 1
 done
 echo "MariaDB is ready"
 
+# dev null to erase errors
+
 if ! mysql -u root -p"$DB_ROOT_PASSWORD" -e "USE $DB_NAME;" 2>/dev/null; then
-    echo "Running first-time database setup..."
+    echo "Setting up database..."
 
     mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
@@ -29,10 +30,11 @@ GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-    echo "Database setup complete"
+    echo "Database complete"
 fi
 
 mysqladmin -u root -p"$DB_ROOT_PASSWORD" shutdown
 wait $MYSQL_PID
 
 exec mysqld --user=mysql --console
+#becoming pid 1
