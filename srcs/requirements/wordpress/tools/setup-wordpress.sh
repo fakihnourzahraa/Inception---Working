16fec2b94df4035b8676a5456437a2bfaf472080
Nour Al Zahraa Fakih <nfakih@c2r6s8.42beirut.com>
2026-08-03 17:05:26 +0300

    passwords changed

--- srcs/requirements/wordpress/tools/setup-wordpress.sh
+++ srcs/requirements/wordpress/tools/setup-wordpress.sh
@@ -14,13 +14,13 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
     echo "Creating wp-config.php..."
     wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" \
-        --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST" \
+        --dbpass="$(cat /run/secrets/db_password)" --dbhost="$DB_HOST" \
         --allow-root
 
     echo "Installing WordPress..."
     wp core install \
         --url="$WP_URL"  --title="$WP_TITLE" \
-        --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_user="$WP_ADMIN_USER" --admin_password="$(cat /run/secrets/wp_admin_password)" \
         --admin_email="$WP_ADMIN_EMAIL" --skip-email \
         --allow-root
         #automatically it wants to send a welcome email
